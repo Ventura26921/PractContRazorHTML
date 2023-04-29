0fcaa000de341749ec38d7cb499de50d35159544
Ventura26921 <gilberto.ventura@catolica.edu.sv>
2023-04-28 18:52:52 -0600

    Agregar archivos de proyecto.

---
+++ Models/equiposDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PractContRazorHTML.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
---
+++ Models/estados_equipos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PractContRazorHTML.Models
+{
+    public class estados_equipo
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id_estados_equipo { get; set; }
+        [Display(Name = "Descripcion")]
+        public string? descripcion { get; set; }
+        [Display(Name = "Estado")]
+        public string? estado { get; set; }
+    }
+}
